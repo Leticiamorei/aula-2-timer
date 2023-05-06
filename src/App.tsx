740e6f0cb05a59f5914e3b1519214555d8b19e43
Leticiamorei <leticiamorei@gmail.com>
2023-05-06 00:03:10 -0300

    add. arquivos

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { Button } from "./components/Button";
+
+
+export function App() {
+  
+  return (
+    <>
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+    </>
+    
+    
+  )
+    
+      
+}
