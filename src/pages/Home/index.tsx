66f0e756581932153f3400634b57d636a87ab1a3
Leticiamorei <leticiamorei@gmail.com>
2023-05-11 23:47:30 -0300

    feat: validação

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,12 +2,21 @@ import { Play } from "phosphor-react";
 import { CountdownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartCountdownButton, TaskInput } from "./styles";
 
 import {useForm} from "react-hook-form"
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
 //controlled / uncrontolled
 //mantem em tempo real a informação do usuario
 
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa'),
+  minutesAmount: zod.number().min(5).max(60),  
+})
+
 
 export function Home() {
-  const { register, handleSubmit, watch } = useForm();
+  const { register, handleSubmit, watch } = useForm({
+    resolver: zodResolver(newCycleFormValidationSchema),
+  });
 
   function handleCreateNewCycle(data: any) {
     console.log(data)
